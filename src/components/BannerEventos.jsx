6fd7cb39def7738f074f10594bf5a6573aa77ea9
viniciusbrito1 <viniciusbrito13@hotmail.com>
2023-05-16 23:58:58 -0300

    V1.3
    Adicionado um botão linkavel no bannerEventos

--- src/components/BannerEventos.jsx
+++ src/components/BannerEventos.jsx
@@ -1,6 +1,18 @@
 import Image from "next/image"
+import Link from "next/link"
 
-export default function BannerEventos(props, {children}){
+export default function BannerEventos(props){
+    let linkDecoration={
+        textDecoration: 'none',
+        color: 'black',
+        height: 'fit-content',
+        width: 'fit-content',
+        backgroundColor: '#E8CA00',
+        padding: '1.7%',
+        borderRadius: '20px',
+        textTransform: 'uppercase',
+        fontWeight: '600'
+    }
     return (
         <divBanner style={{margin: 0}}>
             <divImagem>
@@ -16,6 +28,9 @@ export default function BannerEventos(props, {children}){
             <divTexto>
                 <titulo>{props.titulo}</titulo>
                 <descricao>{props.descricao}</descricao>
+                <linkavel>
+                    <Link href="/" style={linkDecoration}>confira as fotos do último evento</Link>
+                </linkavel>
             </divTexto>
 
             <style jsx>{`
@@ -40,33 +55,43 @@ export default function BannerEventos(props, {children}){
                     display: flex;
                     flex-direction: column;
                     justify-content: center;
-                    gap: 1vw;
+                    gap: 1.2vw;
                     text-align: justify;
                     
                 }
                 titulo{
-                    font-size: 4vw;
-                    font-family: Graduate;
+                    font-family: "Helvetica", sans-serif;
+                    font-size: 5vw;
+                    text-transform: uppercase;
+                    letter-spacing: 2px;
+                    text-shadow: 2px 2px 4px black;
+                    font-weight: 800;
                 }
                 descricao{
                     font-size: 1.2vw;
+                }
+                linkavel{
+                    font-size: 1vw;
                 }
                 @media screen and (max-width: 768px) {
                     divBanner{
                         flex-direction: column;
                         padding: 5%;
-                        gap: 2vw;
+                        gap: 4vw;
                     }
                     divTexto{
                         width: 90vw;
-                        gap: 2vw;
+                        gap: 4vw;
                     }
                     titulo{
                         text-align: center;
                         font-size: 8vw;
-                        font-family: Graduate;
                     }
                     descricao{
+                        font-size: 3.5vw;
+                    }
+                    linkavel{
+                        text-align: center;
                         font-size: 3.5vw;
                     }
                 }
